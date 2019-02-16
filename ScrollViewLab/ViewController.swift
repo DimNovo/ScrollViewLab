b427fc4b1a4a44570043f4cc424aba99190ea70e
Dmitry Novosyolov <i1710@icloud.com>
2019-02-16 11:26:28 +0200

    Initial Commit

---
+++ ScrollViewLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollViewLab
+//
+//  Created by Dmitry Novosyolov on 16/02/2019.
+//  Copyright © 2019 Dmitry Novosyolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
