485f0ec1c05d264f39274ae7b109865116da189d
Dmitry Novosyolov <i1710@icloud.com>
2019-02-16 12:09:53 +0200

    🧐 commit 🧐

--- ScrollViewLab/ViewController.swift
+++ ScrollViewLab/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var scrollImageView: UIImageView!
+    
+    // MARK: - ViewController Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollImageView.image = UIImage(named: "siv")
+        scrollView.delegate = self
     }
+}
 
-
+// MARK: - Custom Methods
+extension ViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        
+        guard scrollView.minimumZoomScale >= 0
+            else
+        {
+            scrollView.minimumZoomScale = 1
+            scrollView.maximumZoomScale = 5
+            scrollView.clipsToBounds = true
+            
+            return scrollImageView
+        }
+        
+        scrollView.maximumZoomScale = 5
+        scrollView.clipsToBounds = true
+        
+        return scrollImageView
+    }
 }
 
